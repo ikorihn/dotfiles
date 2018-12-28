811900d06d7d9795fb90a724cfb364b9f2fed062
ibisha <16367098+ibisha@users.noreply.github.com>
2018-12-28 14:44:04 +0900

    update

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# brew
+
+# pip 
+
+# node
+curl -L git.io/nodebrew | perl - setup
+nodebrew install-binary latest
+npm i -g @google/clasp
+npm i -g @types/google-apps-script
+npm i -g npm
+npm i -g npm-check-updates
+npm i -g typescript
