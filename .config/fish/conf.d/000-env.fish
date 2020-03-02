03667cdf063ad18ab178e70d251eb2164a57dad6
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2020-03-02 13:37:54 +0900

    add fish

---
+++ .config/fish/conf.d/000-env.fish
@@ -0,0 +1,51 @@
+set -U fish_user_paths /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+
+# NeoVim
+set XDG_CONFIG_HOME ~/.config
+
+# Java
+set -xU JAVA_HOME (/System/Library/Frameworks/JavaVM.framework/Versions/A/Commands/java_home)
+set -U fish_user_paths $JAVA_HOME/bin $fish_user_paths
+
+# Android
+set -xU ANDROID_SDK_HOME $HOME/Library/Android/sdk
+set -U fish_user_paths $ANDROID_SDK_HOME/platform-tools $ANDROID_SDK_HOME/tools $fish_user_paths
+
+# Node.js
+set -U fish_user_paths $HOME/.nodebrew/current/bin $fish_user_paths
+
+# Go
+set -U fish_user_paths (go env GOPATH)/bin $fish_user_paths
+
+# Python
+if command -v pyenv 1>/dev/null 2>&1
+  status --is-interactive; and source (pyenv init -|psub)
+  set -U fish_user_paths $HOME/.pyenv/shims $fish_user_paths
+  set -xU PIPENV_VENV_IN_PROJECT 1
+end
+
+# Ruby
+if command -v rbenv 1>/dev/null 2>&1
+  status --is-interactive; and source (rbenv init -|psub)
+  set -U fish_user_paths $HOME/.rbenv/shims $fish_user_paths
+end
+
+set -U fish_user_paths \
+    /usr/local/opt/coreutils/libexec/gnubin \
+    /usr/local/opt/ed/libexec/gnubin \
+    /usr/local/opt/findutils/libexec/gnubin \
+    /usr/local/opt/gnu-sed/libexec/gnubin \
+    /usr/local/opt/gnu-tar/libexec/gnubin \
+    /usr/local/opt/grep/libexec/gnubin \
+    $fish_user_paths
+
+set -xU manpath \
+    /usr/local/opt/coreutils/libexec/gnuman \
+    /usr/local/opt/ed/libexec/gnuman \
+    /usr/local/opt/findutils/libexec/gnuman \
+    /usr/local/opt/gnu-sed/libexec/gnuman \
+    /usr/local/opt/gnu-tar/libexec/gnuman \
+    /usr/local/opt/grep/libexec/gnuman \
+    $manpath
+
+set -xU POWERLINE_ROOT (python -c 'import site; print (site.getsitepackages()[0])')/powerline
---
+++ .config/fish/config.fish
@@ -0,0 +1,115 @@
+# powerlineを有効化
+powerline-daemon -q
+source $POWERLINE_ROOT/bindings/fish/powerline-setup.fish
+
+########################################
+# エイリアス
+
+alias ls 'ls --color=auto -h'
+alias ll 'ls -al'
+
+alias rm 'rm -i'
+alias cp 'cp -i -p'
+alias mv 'mv -i'
+
+alias vim 'nvim'
+alias view 'vim -R'
+
+# sudo の後のコマンドでエイリアスを有効にする
+alias sudo 'sudo '
+
+
+########################################
+# 関数
+function fbr
+  git branch -vv | fzf +m | awk '{print $1}' | sed "s/.* //" | xargs -I{} git switch {}
+end
+
+function fbrm
+  git branch -r | grep -v 'HEAD' | fzf +m | sed "s/.* //" | sed "s#origin/##" | xargs -I{} git switch {}
+end
+
+function fbrd
+  git branch -vv | fzf -m | awk '{print $1}' | sed "s/.* //" | xargs -I{} git branch -D {}
+end
+
+function fgr
+  git log --graph --color=always \
+      --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" |
+  fzf --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort \
+      --bind "ctrl-m:execute:
+                (grep -o '[a-f0-9]\{7\}' | head -1 |
+                xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF'
+                {}
+                FZF-EOF"
+end
+
+function fadd
+  git status --short |
+  awk '{if (substr($0,2,1) !~ / /) print $2}' |
+  fzf --bind "ctrl-d:execute:
+              git diff --color=always {} | less -R
+             " \
+      --bind "ctrl-m:execute:
+              git add {}
+             "
+end
+
+function git_count
+  set author argv[1]
+  set start_date argv[2]
+  set end_date argv[3]
+  git log --numstat --pretty="%H" --author="$author" --since=$start_date --until=$end_date --no-merges | awk 'NF==3 {plus+=$1; minus+=$2} END {printf("%d (+%d, -%d)\n", plus+minus, plus, minus)}'
+end
+
+function fvim
+  set file (find . -type f | fzf-tmux +m)
+  if [ -f "$file" ]
+    nvim $file
+  end
+end
+
+## Android
+function adb_screencap
+  set DATE_TIME `date +"%Y%m%d-%H%M%S"`
+  set FILE_NAME $DATE_TIME.png
+   
+  adb shell screencap -p /sdcard/$FILE_NAME
+  pushd ~/Desktop
+  adb pull /sdcard/$FILE_NAME
+  adb shell rm /sdcard/$FILE_NAME
+   
+  mogrify -resize 300x -unsharp 2x1.4+0.5+0 \
+          -colors 65 -quality 100 -verbose \
+          ~/Desktop/$FILE_NAME
+
+  popd
+end
+
+function delete_DSStore
+  find . -name ".DS_Store" -delete
+end
+
+function urldecode
+  nkf -w --url-input
+end
+
+function urlencode
+  nkf -WwMQ | sed 's/=$//g' | tr "=" "%" | tr -d "\n" |
+    sed -e 's/%7E/~/g' \
+        -e 's/%3D/=/g' \
+        -e 's/%3F/?/g' \
+        -e 's/%5F/_/g' \
+        -e 's/%22/"/g' \
+        -e 's/%2C/,/g' \
+        -e 's/%2D/-/g' \
+        -e 's/%2E/./g'
+end
+
+function urlsort
+  tr "&" "\n" | tr "?" "\n" | sort
+end
+
+source ~/.local_functions
+
+source ~/.iterm2_shell_integration.(basename $SHELL)
