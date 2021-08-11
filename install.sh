cf14cb0c235a86a143eb5bcd949f1288a483e955
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2021-08-11 10:56:53 +0900

    install script

--- install.sh
+++ install.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
 # symlink
-ln -sf ~/.dotfiles/.gitconfig ~/
-ln -sf ~/.dotfiles/.tigrc ~/
-ln -sf ~/.dotfiles/.tmux.conf ~/
-ln -sf ~/.dotfiles/.vimrc ~/
-ln -sf ~/.dotfiles/.zshenv ~/
-ln -sf ~/.dotfiles/.zshrc ~/
+ln -sf ~/dotfiles/.gitconfig ~/
+ln -sf ~/dotfiles/.git_commit_template ~/
+ln -sf ~/dotfiles/.tigrc ~/
+ln -sf ~/dotfiles/.tmux.conf ~/
+ln -sf ~/dotfiles/.vimrc ~/
+ln -sf ~/dotfiles/.zshenv ~/
+ln -sf ~/dotfiles/.zsh ~/
+ln -sf ~/dotfiles/.vim ~/
 mkdir -p ~/.config
-ln -sf ~/.dotfiles/.config/karabiner ~/.config/
-ln -sf ~/.dotfiles/.config/powerline ~/.config/
-ln -sf ~/.dotfiles/.config/nvim ~/.config/
-ln -sf ~/.dotfiles/.vim ~/
+ln -sf ~/dotfiles/.config/karabiner ~/.config/
+ln -sf ~/dotfiles/.config/nvim ~/.config/
+ln -sf ~/dotfiles/.config/starship.toml ~/.config/
 
 # brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle install --file=Mac/Brewfile
 
 # python
-pyenv install 3.8.1
-pyenv global 3.8.1
+LATEST_PYTHON=$(pyenv install --list | rg "^\s+([0-9]+\.?){3}$" | sed 's/\s\+//' | tail -1)
+
+pyenv install $LATEST_PYTHON
+pyenv global $LATEST_PYTHON
 pip install neovim
 pip install powerline-gitstatus
 pip install powerline-status
-pip install awscli
 
 # node
 nodebrew install-binary latest
 nodebrew use latest
-
-source ~/.zshenv
-source ~/.zshrc
