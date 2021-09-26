e338bcea1f0350ef3b8a54e8aa7bd564e5c2f901
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2021-09-26 18:29:29 +0900

    zsh sshで色を変える

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -332,6 +332,26 @@ imgpbcopy() {
   fi
 }
 
+function ssh() {
+  # tmux起動時
+  if [[ -n $(printenv TMUX) ]] ; then
+      # 現在のペインIDを記録
+      local pane_id=$(tmux display -p '#{pane_id}')
+      # 接続先ホスト名に応じて背景色を切り替え
+      if [[ $(echo $1 | grep -e 'btc' -e 'ap' -e 'ntj') ]] ; then
+          tmux select-pane -P 'bg=colour52,fg=white'
+      fi
+
+      # 通常通りssh続行
+      command ssh $@
+
+      # デフォルトの背景色に戻す
+      tmux select-pane -t $pane_id -P 'default'
+  else
+      command ssh $@
+  fi
+}
+
 ########################################
 # Bind keys
 ########################################
