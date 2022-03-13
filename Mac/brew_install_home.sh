ca96b27d04adcdab50f83be83821ca9ea705d26c
ikorihn <16367098+ikorihn@users.noreply.github.com>
2022-03-13 18:53:28 +0900

    brew install scriptを追加
    Brewfileは自分が入れたものなのか依存関係で入ったものなのかわからなくて気持ち悪かったため

---
+++ Mac/brew_install_home.sh
@@ -0,0 +1,73 @@
+#!/bin/zsh
+
+brew install bat
+brew install ccls
+brew install clang-format
+brew install coreutils
+brew install diffutils
+brew install direnv
+brew install emojify
+brew install exa
+brew install fd
+brew install ffmpeg
+brew install findutils
+brew install fzf
+brew install gawk
+brew install gcc
+brew install gh
+brew install ghq
+brew install gibo
+brew install git
+brew install git-delta
+brew install git-secrets
+brew install gnu-getopt
+brew install gnu-sed
+brew install gnutls
+brew install go
+brew install gradle
+brew install gradle-completion
+brew install graphviz
+brew install gron
+brew install groovy
+brew install imagemagick
+brew install jq
+brew install kotlin
+brew install llvm
+brew install mas
+brew install maven
+brew install neovim
+brew install nkf
+brew install openapi-generator
+brew install openjdk
+brew install pet
+brew install pidcat
+brew install plantuml
+brew install python3
+brew install rclone
+brew install rename
+brew install ripgrep
+brew install rsync
+brew install sqlite
+brew install starship
+brew install tesseract
+brew install tig
+brew install tmux
+brew install tmux-xpanes
+brew install tokei
+brew install tree-sitter
+brew install ttygif
+brew install ttyrec
+brew install zsh-completions
+
+brew install --cask authy
+brew install --cask bitwarden
+brew install --cask font-cica
+brew install --cask font-hackgen
+brew install --cask font-hackgen-nerd
+brew install --cask google-chrome
+brew install --cask docker
+brew install --cask karabiner-elements
+brew install --cask obsidian
+brew install --cask raycast
+brew install --cask vivaldi
+brew install --cask visual-studio-code
--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Dot Files
+
+```shell
+# symlink
+ln -sf ~/dotfiles/.tigrc ~/
+ln -sf ~/dotfiles/.tmux.conf ~/
+ln -sf ~/dotfiles/.vimrc ~/
+ln -sf ~/dotfiles/.zshenv ~/
+ln -sf ~/dotfiles/.zsh ~/
+ln -sf ~/dotfiles/.vim ~/
+
+mkdir -p ~/.config
+ln -sf ~/dotfiles/.config/alacritty ~/.config/
+ln -sf ~/dotfiles/.config/karabiner ~/.config/
+ln -sf ~/dotfiles/.config/nvim ~/.config/
+ln -sf ~/dotfiles/.config/git ~/.config/
+ln -sf ~/dotfiles/.config/starship.toml ~/.config/
 ```
-clone https://github.com/ibisha/dotfiles ~/dotfiles
-ln -s dotfiles/.vimrc .vimrc
-ln -s dotfiles/.gvimrc .gvimrc
-ln -s dotfiles/.zshrc .zshrc
-```
+
+## Mac
+
+- [Homebrew](https://brew.sh/index_ja)
+- [Rust](https://www.rust-lang.org/tools/install)
+- [zinit](https://github.com/zdharma-continuum/zinit)
+
