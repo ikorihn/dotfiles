8fb8f51349d8f42718f815f04834c517e7b061ff
ikorihn <16367098+ikorihn@users.noreply.github.com>
2022-08-17 11:55:48 +0900

    neovim: load plugin setting explicitly

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,3 +10,7 @@ for k, v in pairs(modules) do
   package.loaded[v] = nil
   require(v)
 end
+
+for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/pluginconfig", [[v:val =~ '\.lua$']])) do
+  require("pluginconfig/" .. file:gsub("%.lua$", ""))
+end
