3e7078c53eae80cb71e7c02566b565ab2283eef7
ikorihn <16367098+ikorihn@users.noreply.github.com>
2023-02-16 11:10:56 +0900

    nvim: telescope add send to quickfix, enable multi_select

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -117,13 +117,14 @@ function multi_select()
   hidden = true,
   follow = true,
   }
-  return require("telescope.builtin").find_files(opts_ff)
+  return opts_ff
 end
 keymap('n', '<leader>f,', ":Telescope ")
-keymap('n', '<leader>ff', multi_select, opts)
-keymap("n", "<leader>f.", "<cmd>lua require('telescope.builtin').find_files({ search_dirs = { '~/.config/nvim', '~/.config/zsh', '~/.config/karabiner', '~/.config/wezterm' }, hidden = true, follow = true })<cr>", opts)
-keymap("n", "<leader>fg", ":Telescope git_files<CR>", opts)
-keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+keymap('n', '<leader>ff', "<cmd>lua require('telescope.builtin').find_files(multi_select())<cr>", opts)
+keymap("n", "<leader>f.", "<cmd>lua require('telescope.builtin').find_files({ search_dirs = { '~/.config/nvim', '~/.config/zsh', '~/.config/alacritty', '~/.config/tig', '~/.config/karabiner', '~/.config/wezterm' }, hidden = true, follow = true })<cr>", opts)
+keymap("n", "<leader>ft", "<cmd>lua require('telescope.builtin').grep_string{ path_display = { 'smart' }, word_match = '-w', only_sort_text = true, search = '' }<cr>", opts)
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fh", ":Telescope command_history<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 keymap('n', 'ga.', '<cmd>TextCaseOpenTelescope<CR>', { desc = "Telescope" })
