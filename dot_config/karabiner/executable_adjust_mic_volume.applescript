134b362fdfdcc870d587143ba9c225ae9d845d8e
ikorihn <16367098+ikorihn@users.noreply.github.com>
2023-10-10 14:41:54 +0900

    update: misc

--- dot_config/karabiner/executable_adjust_mic_volume.applescript
+++ dot_config/karabiner/executable_adjust_mic_volume.applescript
@@ -1,15 +1,5 @@
 #!/usr/bin/osascript
 
-# Required parameters:
-# @raycast.schemaVersion 1
-# @raycast.title Adjust mic volume
-# @raycast.mode compact
-
-# Optional parameters:
-# @raycast.icon 🎤
-# @raycast.packageName Mic
-# @raycast.argument1 { "type": "text", "placeholder": "volume" }
-
 on run argv
     set micVolume to (item 1 of argv)
     adjustVolume(micVolume)
--- dot_config/nvim/lua/pluginconfig/indentline.lua
+++ dot_config/nvim/lua/pluginconfig/indentline.lua
@@ -6,27 +6,4 @@ end
 vim.cmd [[highlight IndentBlanklineIndent1 guifg=#567692 guibg=#2f2f2f gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent2 guifg=#567692 guibg=#333333 gui=nocombine]]
 
-indent_blankline.setup {
-  space_end_of_line = true,
-  space_char_blankline = "",
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
-  use_treesitter = true,
-  show_current_context = true,
-  buftype_exclude = { "terminal", "nofile" },
-  filetype_exclude = {
-    "help",
-    "packer",
-    "NvimTree",
-  },
-  char = "",
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-    "IndentBlanklineIndent2",
-  },
-  space_char_highlight_list = {
-    "IndentBlanklineIndent1",
-    "IndentBlanklineIndent2",
-  },
-}
-
+indent_blankline.setup()
