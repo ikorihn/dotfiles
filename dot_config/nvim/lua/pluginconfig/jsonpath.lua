f7bf69ddce7dad3827e3df21e65efdcfc7778c3f
ikorihn <16367098+ikorihn@users.noreply.github.com>
2023-10-10 19:15:00 +0900

    nvim: add jsonpath plugin

---
+++ dot_config/nvim/lua/pluginconfig/jsonpath.lua
@@ -0,0 +1,14 @@
+local status_ok, jsonpath = pcall(require, "jsonpath")
+if not status_ok then
+  return
+end
+
+-- show json path in the winbar
+if vim.fn.exists("+winbar") == 1 then
+  vim.opt_local.winbar = "%{%v:lua.require'jsonpath'.get()%}"
+end
+
+-- send json path to clipboard
+vim.keymap.set("n", "y<C-p>", function()
+  vim.fn.setreg("+", jsonpath.get())
+end, { desc = "copy json path", buffer = true })
