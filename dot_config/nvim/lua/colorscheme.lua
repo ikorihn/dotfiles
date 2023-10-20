7b8382d123a508e8285a6bf5fa1467676669d64e
ikorihn <16367098+ikorihn@users.noreply.github.com>
2023-10-20 23:02:42 +0900

    nvim: update colorscheme

--- dot_config/nvim/lua/colorscheme.lua
+++ dot_config/nvim/lua/colorscheme.lua
@@ -1,24 +1,24 @@
-local colorscheme = "gruvbox"
+-- local scheme_status_ok, gruvbox = pcall(require, "gruvbox")
+-- if not scheme_status_ok then
+--   return
+-- end
+-- 
+-- gruvbox.setup({
+--   undercurl = true,
+--   underline = true,
+--   bold = true,
+--   italic = {},
+--   strikethrough = true,
+--   invert_selection = false,
+--   invert_signs = false,
+--   invert_tabline = false,
+--   invert_intend_guides = false,
+--   inverse = true, -- invert background for search, diffs, statuslines and errors
+--   contrast = "", -- can be "hard", "soft" or empty string
+--   overrides = {},
+-- })
 
-local scheme_status_ok, gruvbox = pcall(require, "gruvbox")
-if not scheme_status_ok then
-  return
-end
-
-gruvbox.setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = {},
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  overrides = {},
-})
+local colorscheme = "tokyonight-moon"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
