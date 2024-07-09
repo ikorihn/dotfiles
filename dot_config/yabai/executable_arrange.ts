1f99ee27ce96cdefdc3917563bbd3a5d958bc1ce
ikorihn <16367098+ikorihn@users.noreply.github.com>
2024-07-09 18:41:33 +0900

    yabai: 左右二分割にウィンドウを整列するコマンド

--- dot_config/yabai/executable_arrange.ts
+++ dot_config/yabai/executable_arrange.ts
@@ -1,18 +1,26 @@
 #!bun run
 
 import { $ } from "bun";
+import { parseArgs } from "node:util";
 
 const RIGHT_HALF = "16:10:5:0:5:16";
 const LEFT_HALF = "16:10:0:0:5:16";
 const TOP_RIGHT = "16:10:5:0:5:8";
 const BOTTOM_RIGHT = "16:10:5:8:5:8";
+const RIGHT_70 = "16:10:7:0:3:16";
 const LEFT_70 = "16:10:0:0:7:16";
-const RIGHT_70 = "16:10:3:0:7:16";
+const LEFT_60 = "16:20:0:0:11:16";
+const RIGHT_40 = "16:20:11:0:9:16";
 const FULL = "1:1:0:0:1:1";
 
 function split(leftPercent: number, overwrap: number = 0) {
-  return [`16:100:0:0:${leftPercent+overwrap}:16`, `16:100:${leftPercent-overwrap}:0:${100-leftPercent+overwrap}:16`];
+  return [`16:100:0:0:${leftPercent + overwrap}:16`, `16:100:${leftPercent - overwrap}:0:${100 - leftPercent + overwrap}:16`];
 }
+
+const width = Number.parseInt(await $`yabai -m query --displays --display | jq '.frame.w | floor'`.text());
+const percent = width < 3000 ? split(50, 10) : split(50);
+const left = percent[0];
+const right = percent[1];
 
 interface Window {
   id: number;
@@ -23,28 +31,17 @@ interface Window {
 }
 
 async function arrange_float() {
-  const width =
-    await $`yabai -m query --displays --display | jq '.frame.w | floor'`.text();
   const windows = (await $`yabai -m query --windows`.json()) as Window[];
-
-  const percent = split(50);
-  const left = percent[0];
-  const right = percent[1];
-  console.log(left, right);
 
   let i = 0;
   const gridmap = new Map();
   for (const window of windows) {
-    if (
-      /System Settings|Karabiner.*|Finder|Bitwarden|KeePassXC|カレンダー/.test(
-        window.app,
-      )
-    ) {
+    if (/System Settings|Karabiner.*|Finder|Bitwarden|KeePassXC|カレンダー/.test(window.app)) {
       console.log(window.app, "ignore");
       continue;
     }
 
-    if (parseInt(width) < 2000) {
+    if (width < 2000) {
       await $`yabai -m window ${window.id} --grid ${FULL}`;
       continue;
     }
@@ -69,10 +66,29 @@ async function arrange_float() {
         }
     }
 
-    const grid = gridmap.get(window.app)
+    const grid = gridmap.get(window.app);
     console.log(window.app, grid);
     await $`yabai -m window ${window.id} --grid ${grid}`;
   }
 }
 
-await arrange_float();
+const { values } = parseArgs({
+  args: Bun.argv.slice(2),
+  options: {
+    position: {
+      type: "string",
+      short: "p",
+    },
+  },
+  strict: true,
+});
+
+const position = values.position;
+
+if (position === "left") {
+  await $`yabai -m window --grid ${left}`;
+} else if (position === "right") {
+  await $`yabai -m window --grid ${right}`;
+} else {
+  await arrange_float();
+}
