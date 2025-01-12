130fe02be9bf7f2f437996144f43f3a9d8bca944
ikorihn <16367098+ikorihn@users.noreply.github.com>
2025-01-13 00:40:05 +0900

    termux dailynote

--- termux/obsidian_dailynote.sh
+++ termux/obsidian_dailynote.sh
@@ -1,4 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 cd $HOME/storage/shared/memo
-sed -e "s/{{date}}/$(date '+%Y-%m-%d')/" -e "s/{{time}}/$(date '+%H:%M')/" daily/daily_template.md
+sed -e "s/{{date}}/$(date '+%Y-%m-%d')/" -e "s/{{time}}/$(date '+%H:%M')/" daily/daily_template.md > "daily/$(date '+%Y-%m-%d').md"
