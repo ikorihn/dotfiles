96ea051d168f39d2eeb17f1d8ed879c66f6a3dac
ikorihn <16367098+ikorihn@users.noreply.github.com>
2026-06-22 15:51:39 +0900

    mac: fix defaults

--- Mac/defaults/file.sh
+++ Mac/defaults/file.sh
@@ -1,5 +1,3 @@
-sudo chflags nohidden /Volumes # /Volumes ディレクトリを見えるようにする
-chflags nohidden ~/Library # ~/Library ディレクトリを見えるようにする
 defaults write com.apple.finder AnimateWindowZoom -bool false # フォルダを開くときのアニメーションを無効にする
 defaults write com.apple.finder AppleShowAllFiles YES # 不可視ファイルを表示する
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf" # 検索時にデフォルトでカレントディレクトリを検索する
@@ -15,7 +13,6 @@ defaults write com.apple.finder ShowPathbar -bool true # パスバーを表示�
 defaults write com.apple.finder ShowStatusBar -bool true # ステータスバーを表示する
 defaults write com.apple.finder ShowTabView -bool true # タブバーを表示する
 defaults write com.apple.finder ShowRecentTags -bool false # Recent Tags
-defaults write com.apple.finder WarnOnEmptyTrash -bool false # ゴミ箱を空にする前の警告を無効化する
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true # Finder のタイトルバーにフルパスを表示する
 defaults write com.apple.finder _FXSortFoldersFirst -bool true # 名前で並べ替えを選択時にディレクトリを前に置くようにする
 
@@ -29,10 +26,3 @@ defaults write -g AppleShowAllExtensions -bool true # 全ての拡張子のフ�
 # USB やネットワークストレージに .DS_Store ファイルを作成しない
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-
-# スクリーンショット
-defaults write com.apple.screencapture name "screenshot" # 名前を変更
-# defaults write com.apple.screencapture include-date -bool false
-defaults write com.apple.screencapture disable-shadow -bool true # スクリーンキャプチャの影をなくす
-defaults write com.apple.screencapture type -string "png" # スクリーンショットの保存形式を PNG にする
-defaults write com.apple.screencapture location -string "~/Pictures/screenshot" # スクリーンショットの保存先
