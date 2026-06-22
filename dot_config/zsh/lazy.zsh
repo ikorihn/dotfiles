5a997bf8483335d85268e040221598cfec673af7
ikorihn <16367098+ikorihn@users.noreply.github.com>
2026-06-22 18:18:06 +0900

    zsh: 読み込みタイミングごとにファイルまとめる

--- dot_config/zsh/lazy.zsh
+++ dot_config/zsh/lazy.zsh
@@ -663,3 +663,53 @@ zle -N fbrm
 bindkey '^G^M' fbrm
 zle -N petcopy
 bindkey '^G^P' petcopy
+
+########################################
+# Lazy abbreviations
+########################################
+# zsh-abbr 読み込み後でよい展開設定。
+#abbr -S -q -f rm='trash -F'
+#abbr -S -q rmr='rm -r'
+
+# abbr cp='cp -i -p'
+# abbr mv='mv -i'
+abbr -S -q -f mkdir='mkdir -p'
+
+abbr -S -q --force dc='cd' > /dev/null
+abbr -S -q ':q'='exit'
+abbr -S -q -f 'ex'='exit'
+abbr -S -q fzfcol="| fzf +m | cut -d ' ' -f 1"
+abbr -g -q devnull='>/dev/null 2>&1'
+abbr -g -q pc='| pbcopy'
+abbr -g -q L='| less'
+abbr -S -q vimcon='vim ~/.config/**'
+abbr -g -q xn='| xargs nvim'
+
+abbr -S -q lll='ll $(which'
+
+abbr -S -q tcap="tmux capture-pane -p -S -32768 | nvim + -"
+
+# クリップボードにコピーしつつ標準出力
+abbr -S -q teee='tee >(pbcopy)'
+
+# git
+abbr -S -q g='git'
+abbr -S -q gca='git commit --amend'
+abbr -S -q gcim='git commit -m'
+abbr -S -q gdn='git diff --name-status origin/master'
+abbr -S -q gp='git pull --rebase --autostash --force'
+abbr -S -q gpu='git push'
+abbr -S -q gre='git rebase origin/master --autostash'
+abbr -S -q gres='git restore .'
+abbr -S -q grh='git reset HEAD\^'
+abbr -S -q -f gs='git status'
+abbr -S -q gsn='git show --name-status'
+abbr -S -q gst='git stash'
+abbr -S -q gstp='git stash pop'
+abbr -S -q gsw='git switch -c feature/'
+abbr -S -q t='tig'
+abbr -S -q lg='lazygit'
+
+# ripgrep,fd
+# `!` はescapeされるためescape sequenceで定義 https://github.com/olets/zsh-abbr/issues/84#issuecomment-1475075037
+abbr -S -q rgg="rg --glob='\041*{.pb.go,_test.go,mock_*.go,_gen.go}'"
--- dot_config/zsh/sync.zsh
+++ dot_config/zsh/sync.zsh
@@ -1,4 +1,123 @@
 ########################################
+# Sync shell options
+########################################
+# シェルの基本挙動は最初の入力前に確定させる。
+
+# 色を使用出来るようにする
+autoload -Uz colors
+colors
+
+# emacs 風キーバインドにする
+bindkey -e
+
+# ヒストリの設定
+export HISTFILE=$XDG_CACHE_HOME/zsh/history
+export HISTSIZE=50000
+# export HISTORY_IGNORE="(ls|ll|cd|pwd|exit|cd *|ls *|ll *)"
+export SAVEHIST=40000
+
+# zsh historyに保存するときのhook
+# https://superuser.com/questions/902241/how-to-make-zsh-not-store-failed-command
+# ${(z)} は、zsh の配列を扱うための拡張機能の一つで、指定された文字列を特定の方法で分割して配列に格納するパラメータ展開 (parameter expansion)
+zshaddhistory() {
+  # 存在しないコマンドを保存しない
+  local j=1
+  local only_define=1
+  for in in "${${(z)1}[@]}"; do
+    whence ${${(z)1}[$j]} >| /dev/null && return 0
+    if [[ ${in} == *=* || ${in} == "export" ]]; then
+      ((j++))
+    elif [[ ${in} == ";" ]]; then
+      # ignore
+    else
+      only_define=0
+    fi
+  done
+  if [[ ${only_define} -eq 1 ]]; then
+    return 0
+  fi
+
+  command="${${(z)1}[$j]}"
+  if [[ $command == \~* ]]; then
+    command=${command//\~/${HOME}}
+  fi
+  if [[ -f ${command} ]]; then
+    return 0
+  fi
+  whence ${command} >| /dev/null || return 1
+}
+
+# 単語の区切り文字を指定する
+autoload -Uz select-word-style
+select-word-style default
+# ここで指定した文字は単語区切りとみなされる
+# / も区切りと扱うので、^W でディレクトリ１つ分を削除できる
+zstyle ':zle:*' word-chars " /=;@:{},|"
+zstyle ':zle:*' word-style unspecified
+
+autoload -U zmv
+
+# コマンドラインをEDITORで編集する
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey "^O" edit-command-line
+
+# 補完で小文字でも大文字にマッチさせる
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# ../ の後は今いるディレクトリを補完しない
+zstyle ':completion:*' ignore-parents parent pwd ..
+
+# sudo の後ろでコマンド名を補完する
+zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
+
+# ps コマンドのプロセス名補完
+zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
+
+# 日本語ファイル名を表示可能にする
+setopt print_eight_bit
+
+# beep を無効にする
+setopt no_beep
+
+# フローコントロールを無効にする
+setopt no_flow_control
+
+# Ctrl+Dでzshを終了しない
+setopt ignore_eof
+
+# '#' 以降をコメントとして扱う
+setopt interactive_comments
+
+# cd したら自動的にpushdする
+setopt auto_pushd
+# 重複したディレクトリを追加しない
+setopt pushd_ignore_dups
+
+# 同時に起動したzshの間でヒストリを共有する
+setopt share_history
+# ヒストリに実行時刻をつける
+setopt extended_history
+
+# 同じコマンドをヒストリに残さない
+setopt hist_ignore_all_dups
+
+# スペースから始まるコマンド行はヒストリに残さない
+setopt hist_ignore_space
+
+# ヒストリに保存するときに余分なスペースを削除する
+setopt hist_reduce_blanks
+
+# historyコマンドは記録しない
+# setopt hist_no_store
+
+# 高機能なワイルドカード展開を使用する
+setopt extended_glob
+
+# 明確なドットの指定なしで.から始まるファイルをマッチ
+setopt globdots
+
+########################################
 # Sync aliases
 ########################################
 # 起動直後の入力で使う基本 alias は defer しない。
